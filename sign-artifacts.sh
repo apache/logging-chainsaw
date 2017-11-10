042474aeca70a8b6d9846e26273c3b2957af6a67
Matt Sicker <boards@gmail.com>
2017-11-10 12:42:11 -0600

    Add sign-artifacts.sh helper script

---
+++ sign-artifacts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# script to generate .asc, .md5, and .sha512 files for built artifacts
+
+if [[ $# -ne 1 ]]
+then
+    echo "Usage: $0 gpg-key-id"
+    exit 1
+fi
+
+GPG_KEY="$1"
+
+cd target
+for f in apache-chainsaw-*-bin.* apache-chainsaw-*-standalone.*
+do
+    gpg --default-key="$GPG_KEY" --sign --detach-sign --armor "$f"
+    md5 "$f" >"$f.md5"
+    shasum -a 512 "$f" >"$f.sha512"
+done
