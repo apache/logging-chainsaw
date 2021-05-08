f989733c5720a57db9fcd9978a4d5429247462e8
Robert Middleton <rm5248@users.noreply.github.com>
2021-05-08 19:18:59 -0400

    Disable OSX integration on anything that is not OSX (#8)
    * Fix OSX integration to check to see if it is supported first

--- src/main/java/org/apache/log4j/chainsaw/osx/OSXIntegration.java
+++ src/main/java/org/apache/log4j/chainsaw/osx/OSXIntegration.java
@@ -34,21 +34,28 @@ public class OSXIntegration {
     private static final Desktop desktop = Desktop.getDesktop();
 
     public static final void init(final LogUI logUI) {
-        desktop.setAboutHandler(e ->
-            logUI.showAboutBox()
-        );
+        if( desktop.isSupported(Desktop.Action.APP_ABOUT) ){
+            desktop.setAboutHandler(e ->
+                logUI.showAboutBox()
+            );
+        }
 
-        desktop.setPreferencesHandler(e ->
-            logUI.showApplicationPreferences()
-        );
-        desktop.setQuitHandler((e, r) -> {
-                if (
-                    logUI.exit()) {
-                    r.performQuit();
-                } else {
-                    r.cancelQuit();
+        if( desktop.isSupported(Desktop.Action.APP_PREFERENCES) ){
+            desktop.setPreferencesHandler(e ->
+                logUI.showApplicationPreferences()
+            );
+        }
+
+        if( desktop.isSupported(Desktop.Action.APP_QUIT_HANDLER) ){
+            desktop.setQuitHandler((e, r) -> {
+                    if (
+                        logUI.exit()) {
+                        r.performQuit();
+                    } else {
+                        r.cancelQuit();
+                    }
                 }
-            }
-        );
+            );
+        }
     }
 }
