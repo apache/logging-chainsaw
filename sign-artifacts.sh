32d45cdde7af05089ea25e614f3f2249f3178564
Robert Middleton <robert.middleton@rm5248.com>
2021-06-05 20:33:38 -0400

    Sign the correct source format

--- sign-artifacts.sh
+++ sign-artifacts.sh
@@ -11,7 +11,7 @@ fi
 GPG_KEY="$1"
 
 cd target
-for f in apache-chainsaw-*-bin.* apache-chainsaw-*-standalone.* apache-chainsaw-source*.*
+for f in apache-chainsaw-*-bin.* apache-chainsaw-*-standalone.* apache-chainsaw-*-src.*
 do
     gpg --default-key="$GPG_KEY" --sign --detach-sign --armor "$f"
     shasum -a 512 "$f" >"$f.sha512"
